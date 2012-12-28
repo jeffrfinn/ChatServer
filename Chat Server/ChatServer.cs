e6a917e0eb23bb874723c6a7048470fce8158671
jeff <jeff.r.finn@gmail.com>
2012-12-28 14:44:02 +0000

    initial commit, some work was completed before this commit

---
+++ Chat Server/ChatServer.cs
@@ -0,0 +1,119 @@
+﻿using System.IO;
+using System.Net;
+using System;
+using System.Threading;
+using Chat = System.Net;
+using System.Collections;
+using System.Net.Sockets;
+
+
+
+namespace Chat_Server
+{
+    class ChatServer
+    {
+        TcpListener chatServer;
+        public static Hashtable nickName;
+        public static Hashtable nickNameByConnect;
+        IPAddress host = IPAddress.Parse("localhost");
+
+        public ChatServer()
+        {
+            nickName = new Hashtable(100);
+            nickNameByConnect = new Hashtable(100);
+
+            chatServer = new TcpListener(host, 4296);
+
+            while (true)
+            {
+                chatServer.Start();
+
+                if (chatServer.Pending())
+                {
+                    Chat.Sockets.TcpClient chatConnection = chatServer.AcceptTcpClient();
+
+                    Console.WriteLine("You are now connected");
+
+                    DoCommunicate comm = new DoCommunicate(chatConnection);
+                }
+            }
+        }
+
+        public static void SendMsgToAll(string nick, string msg)
+        {
+            StreamWriter writer;
+
+            ArrayList toRemove = new ArrayList(0);
+
+            Chat.Sockets.TcpClient[] tcpClient = new Chat.Sockets.TcpClient[ChatServer.nickName.Count];
+
+            ChatServer.nickName.Values.CopyTo(tcpClient, 0);
+
+            for (int i = 0; i < tcpClient.Length; i++)
+            {
+                try
+                {
+                    if (msg.Trim() == "" || tcpClient[i] == null)
+                    {
+                        continue;
+
+                        writer.WriteLine(nick + " : " + msg);
+
+                        writer.Flush();
+
+                        writer = null;
+                    }
+                }
+                catch(Exception e44)
+                {
+                    //e44 = e44;
+
+                    string str = (string)ChatServer.nickNameByConnect[tcpClient[i]];
+
+                    ChatServer.SendSystemMessage("** " + str + " ** Has Left The Room.");
+
+                    ChatServer.nickName.Remove(str);
+
+                    ChatServer.nickNameByConnect.Remove(tcpClient[i]);
+                }
+            }
+        }
+
+        public static void SendSystemMessage(string msg)
+        {
+            StreamWriter writer;
+
+            ArrayList toRemove = new ArrayList(0);
+
+            Chat.Sockets.TcpClient[] tcpClient = new Chat.Sockets.TcpClient[ChatServer.nickName.Count];
+
+            ChatServer.nickName.Values.CopyTo(tcpClient, 0);
+
+            for (int i = 0; i < tcpClient.Length; i++)
+            {
+                try
+                {
+                    if (msg.Trim() == "" || tcpClient[i] == null)
+                    {
+                        writer = new StreamWriter(tcpClient[i].GetStream());
+
+                        writer.WriteLine(msg);
+
+                        writer.Flush();
+
+                        writer = null;
+
+                    }
+                }
+                catch(Exception e44)
+                {
+                    //e44 = e44;
+
+                    ChatServer.nickName.Remove(ChatServer.nickNameByConnect[tcpClient[i]]);
+
+                    ChatServer.nickNameByConnect.Remove(tcpClient[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Chat Server/DoCommunicate.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Net;
+using System;
+using System.Threading;
+using Chat = System.Net;
+using System.Collections;
+using Chat_Server;
+using System.Net.Sockets;
+
+
+
+namespace Chat_Server
+{
+    class DoCommunicate
+    {
+        TcpClient client;
+        StreamReader reader;
+        StreamWriter writer;
+
+        String nickName;
+
+        public DoCommunicate(TcpClient tcpClient)
+        {
+            client = tcpClient;
+
+            Thread chatThread = new Thread(new ThreadStart(StartChat));
+
+            chatThread.Start();
+        }
+
+        private string GetNickName()
+        {
+            writer.WriteLine("What is your name ?");
+            writer.Flush();
+
+            return reader.ReadLine();
+        }
+
+        private void StartChat()
+        {
+            reader = new StreamReader(client.GetStream());
+
+            writer = new StreamWriter(client.GetStream());
+
+            writer.WriteLine("Welcome to chat");
+
+            nickName = GetNickName();
+
+            while (Chat_Server.ChatServer.nickName.Contains(nickName))
+            {
+                writer.WriteLine("ERROR Username already exists please try another");
+                nickName = GetNickName();
+            }
+
+            Chat_Server.ChatServer.nickName.Add(nickName, client);
+
+            Chat_Server.ChatServer.nickNameByConnect.Add(client, nickName);
+
+            Chat_Server.ChatServer.SendSystemMessage("** " + nickName + " ** Has joined the room");
+
+            writer.WriteLine("Now Talking.....\r\n-------------------------------");
+
+            writer.Flush();
+
+            Thread chatThread = new Thread(new ThreadStart(RunChat));
+
+            chatThread.Start();
+        }
+
+        private void RunChat()
+        {
+            try
+            {
+                string line = "";
+
+                while (true)
+                {
+                    line = reader.ReadLine();
+
+                    Chat_Server.ChatServer.SendMsgToAll(nickName, line);
+                }
+            }
+            catch (Exception e44)
+            {
+                Console.WriteLine(e44);
+            }
+        }
+    }
+}
